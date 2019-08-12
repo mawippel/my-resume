7b286264d4f167effbafee308b2597e13e673414
Marcelo Wippel <marcelowippel@localhost.localdomain>
2019-08-12 18:05:08 -0300

    feat: link to acclaim badge

--- src/components/CertificationsBadge.js
+++ src/components/CertificationsBadge.js
@@ -3,12 +3,16 @@ import Oca from '../assets/oca-badge.png';
 import Ocp from '../assets/ocp-badge.png';
 
 const CertificationsBadge = () => {
-	return (
-		<div className='certification-container'>
-            <img src={Oca} alt="OCA" className='certification-pic' />
-            <img src={Ocp} alt="OCP" className='certification-pic' />
+    return (
+        <div className='certification-container'>
+            <a href='https://www.youracclaim.com/badges/e314e78b-ea91-4259-b241-71ac27b46ac0'>
+                <img src={Oca} alt="OCA" className='certification-pic' />
+            </a>
+            <a href='https://www.youracclaim.com/badges/866820aa-0629-4948-9b95-4b73f85dfc4e'>
+                <img src={Ocp} alt="OCP" className='certification-pic' />
+            </a>
         </div>
-	);
+    );
 };
 
 export default CertificationsBadge;
