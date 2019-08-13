7418b98b3ebb017d385c5d5f3fddf1fade545688
Marcelo Wippel <marcelowippel@localhost.localdomain>
2019-08-13 20:13:56 -0300

    fixes: css fixes

--- src/components/RightContainer.js
+++ src/components/RightContainer.js
@@ -13,19 +13,22 @@ const RightContainer = () => {
 
 	return (
 		<Grid item xs={8} className="right-container">
+			<div>
+
+			</div>
 			<div className="right-content-container">
 				<div className="job-period">
 					<h1>Marcelo Wippel</h1>
 					<div className="flags">
 						<img
 							src={brFlag}
-							onClick={changeLanguage("pt")}
+							onClick={() => changeLanguage("pt")}
 							alt="Brazil Flag"
 							className="flag"
 						/>
 						<img
 							src={usFlag}
-							onClick={changeLanguage("en")}
+							onClick={() => changeLanguage("en")}
 							alt="US Flag"
 							className="flag"
 						/>
@@ -41,56 +44,62 @@ const RightContainer = () => {
 
 				<h2>{t("exp-title")}</h2>
 				<hr />
-				<div>
-					<h3>{t("exp-occupation-1")}</h3>
-					<div className="job-period">
-						<a href="https://www.teclogica.com.br/">
-							<h4 className="job-period-subtitle">Teclógica</h4>
-						</a>
-						<h4 className="job-period-subtitle">10/2018 - {t("now")}</h4>
+				<div className='experiences'>
+					<div>
+						<h3>{t("exp-occupation-1")}</h3>
+						<div className="job-period">
+							<a href="https://www.teclogica.com.br/">
+								<h4 className="job-period-subtitle">Teclógica</h4>
+							</a>
+							<h4 className="job-period-subtitle">10/2018 - {t("now")}</h4>
+						</div>
+						<p>{t("exp-1-1")}</p>
+						<p>
+							<b>Backend Stack:</b> {t("exp-1-2")}
+						</p>
+						<p>
+							<b>Frontend Stack:</b> {t("exp-1-3")}
+						</p>
 					</div>
-					<p>{t("exp-1-1")}</p>
-					<p>
-						<b>Backend Stack:</b> {t("exp-1-2")}
-					</p>
-					<p>
-						<b>Frontend Stack:</b> {t("exp-1-3")}
-					</p>
-				</div>
-				<hr />
-				<div>
-					<h3>{t("exp-occupation-2")}</h3>
-					<div className="job-period">
-						<h4 className="job-period-subtitle">Pública Tecnologia</h4>
-						<h4 className="job-period-subtitle">08/2016 - 10/2018</h4>
-					</div>
-
-					<p>{t("exp-2-1")}</p>
-					<p>
-						<b>Backend Stack:</b> {t("exp-2-2")}
-					</p>
-					<p>
-						<b>Frontend Stack:</b> {t("exp-2-3")}
-					</p>
-				</div>
-
-				<hr />
-				<div>
-					<h3>{t("exp-occupation-3")}</h3>
-					<div className="job-period">
-						<h4 className="job-period-subtitle">Pública Tecnologia</h4>
-						<h4 className="job-period-subtitle">03/2016 - 08/2016</h4>
-					</div>
-
-					<p>{t("exp-3-1")}</p>
-					<p>
-						<b>Main Stack:</b> {t("exp-3-2")}
-					</p>
-				</div>
-
-				<div>
-					<h2>{t("qualifications-title")}</h2>
 					<hr />
+					<div>
+						<h3>{t("exp-occupation-2")}</h3>
+						<div className="job-period">
+							<a href="http://www.publica.inf.br/">
+								<h4 className="job-period-subtitle">Pública Tecnologia</h4>
+							</a>
+							<h4 className="job-period-subtitle">08/2016 - 10/2018</h4>
+						</div>
+
+						<p>{t("exp-2-1")}</p>
+						<p>
+							<b>Backend Stack:</b> {t("exp-2-2")}
+						</p>
+						<p>
+							<b>Frontend Stack:</b> {t("exp-2-3")}
+						</p>
+					</div>
+
+					<hr />
+					<div>
+						<h3>{t("exp-occupation-3")}</h3>
+						<div className="job-period">
+							<a href="http://www.publica.inf.br/">
+								<h4 className="job-period-subtitle">Pública Tecnologia</h4>
+							</a>
+							<h4 className="job-period-subtitle">03/2016 - 08/2016</h4>
+						</div>
+
+						<p>{t("exp-3-1")}</p>
+						<p>
+							<b>Main Stack:</b> {t("exp-3-2")}
+						</p>
+					</div>
+				</div>
+
+				<h2>{t("qualifications-title")}</h2>
+				<hr />
+				<div className='qualifications'>
 					<p>{t("qualifications-subtitle-1")}</p>
 					<ul>
 						<li>{t("qualifications-desc-1")}</li>
@@ -112,10 +121,10 @@ const RightContainer = () => {
 					</ul>
 				</div>
 
-				<div>
-					<h2>{t('certifications')}</h2>
-					<hr />
-					<ul>
+				<h2>{t('certifications')}</h2>
+				<hr />
+				<div className='languages'>
+					<ul className='no-padding-ul'>
 						<li>Oracle Certified Associate, Java SE 8 Programmer I</li>
 						<li>Oracle Certified Professional, Java SE 8 Programmer II</li>
 						<li>
@@ -131,10 +140,10 @@ const RightContainer = () => {
 					</ul>
 				</div>
 
-				<div>
-					<h2>{t('languages-title')}</h2>
-					<hr />
-					<ul>
+				<h2>{t('languages-title')}</h2>
+				<hr />
+				<div className='languages'>
+					<ul className='no-padding-ul'>
 						<li>{t('languages-1')}</li>
 						<li>{t('languages-2')}</li>
 					</ul>
