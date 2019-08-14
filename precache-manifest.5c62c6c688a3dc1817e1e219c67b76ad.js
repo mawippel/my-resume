2832a45e24ab713bb634d9eadc90964d346d0f75
Marcelo Wippel <mawippel2@hotmail.com>
2019-08-14 10:16:39 -0300

    Updates

---
+++ precache-manifest.5c62c6c688a3dc1817e1e219c67b76ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af31e2175f884baf8d1683e8942fe95c",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "b9ffe789929335d41f0a",
+    "url": "/my-resume/static/css/main.80788ae5.chunk.css"
+  },
+  {
+    "revision": "e8a2733fa1697d6ccb77",
+    "url": "/my-resume/static/js/2.c8de81e8.chunk.js"
+  },
+  {
+    "revision": "b9ffe789929335d41f0a",
+    "url": "/my-resume/static/js/main.465ff878.chunk.js"
+  },
+  {
+    "revision": "bc49de59b6d597d1bd16",
+    "url": "/my-resume/static/js/runtime~main.8f79120b.js"
+  },
+  {
+    "revision": "f591ea75bbaf05b2294c101da304c037",
+    "url": "/my-resume/static/media/Marcelo.f591ea75.jpg"
+  },
+  {
+    "revision": "b804ad7aa0a48317b9d60b3693f41387",
+    "url": "/my-resume/static/media/oca-badge.b804ad7a.png"
+  },
+  {
+    "revision": "4ef4eee06c90fdfd2b2f905b1f0c5c89",
+    "url": "/my-resume/static/media/ocp-badge.4ef4eee0.png"
+  },
+  {
+    "revision": "7d7fbe34ab61ecb4da93bfb602368b7e",
+    "url": "/my-resume/static/media/us-flag.7d7fbe34.png"
+  }
+]);
